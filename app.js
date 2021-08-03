dcf7ba28d199b632ff8a4359dd71c6b0cd374d25
javiercavalero <javicavalero56@gmail.com>
2021-08-03 11:25:40 -0300

    header terminado

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3030;
 
 app.use(express.static("public"));
 
-app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")));
+app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","index.html")));
 
 
 app.listen(port, ()=> console.log(`servidor corriendo en http://localhost:${port}`));
